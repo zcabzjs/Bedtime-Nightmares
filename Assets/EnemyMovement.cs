6c2a51087ed17dc7cf9930670e9527acfa9f9664
Zi Jian Sim <zijian12345@hotmail.com>
2017-11-29 15:30:45 +0000

    Added enemy model, materials, animations and behaviours

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour {
+    GameObject player;
+    NavMeshAgent nav;
+	// Use this for initialization
+	void Awake () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        nav = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if(enemyHealth.currentHealth > 0 && playerHealth > 0)
+        //{
+        nav.SetDestination(player.transform.position);
+        //}
+        //{
+        //  nav.enabled = false;
+        //}
+	}
+}
