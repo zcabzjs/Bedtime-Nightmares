32f04f22b3058aa181b375de949eaf34cbb7149d
Zi Jian Sim <zijian12345@hotmail.com>
2017-11-29 20:37:42 +0000

    Added enemy attacks

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthBar;
+    public Image damageImage;
+    public AudioClip deathClip;
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f); //Colour of the flash(Red) Might change this
+
+    Animator anim;
+    AudioSource playerAudio;
+    PlayerMovement playerMovement;
+    bool isDead;
+    bool isDamaged;
+
+	// Use this for initialization
+	void Awake () {
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        playerMovement = GetComponent<PlayerMovement>();
+        //playerShooting = GetComponentInChildren<PlayerShooting>();
+        currentHealth = startingHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isDamaged)
+        {
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        isDamaged = false;
+    }
+
+    public void TakeDamage(int damageCount)
+    {
+        isDamaged = true;
+        currentHealth -= damageCount;
+        healthBar.value = currentHealth;
+        playerAudio.Play();
+        if(!isDead && currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+        //playerShooting.DisableEffects();
+        anim.SetTrigger("IsDead");
+        playerAudio.clip = deathClip;
+        playerAudio.Play();
+        playerMovement.enabled = false;
+        //playerShooting.enabled = false;
+    }
+}
